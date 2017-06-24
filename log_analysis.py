aefac3c51e2601cdf69ed577c64c49986ee41f3a
Praveen Sankaranarayanan <pravs.91@gmail.com>
2017-06-24 13:17:43 -0700

    pep8 fixes

--- log_analysis.py
+++ log_analysis.py
@@ -25,19 +25,19 @@ def getMostArticleViews():
 
 def getMostAuthorViews():
     query = "SELECT name, sum(views) as count FROM articleViews JOIN authors\
-		on authors.id = articleViews.author group by name\
-		order by count desc;"
+        on authors.id = articleViews.author group by name\
+        order by count desc;"
     rows = executeQuery(query)
-    print "2. The three most popular authors of all time by article views are:\n"
+    print "2. The three most popular authors of all time by article views:\n"
     for row in rows:
         print "\t%s - %s views" % (row[0], row[1])
 
 
 def getErrorDates():
     query = "SELECT requests.time,\
-    	(errors.errors::float / requests.requests) * 100.0\
-		as ratio from requests, errors where requests.time = errors.time\
-		order by ratio desc;"
+        (errors.errors::float / requests.requests) * 100.0\
+        as ratio from requests, errors where requests.time = errors.time\
+        order by ratio desc;"
     rows = executeQuery(query)
     print "3. The days when more than 1% of requests led to errors:\n"
     for row in rows:
